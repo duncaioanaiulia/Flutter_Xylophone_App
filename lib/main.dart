00bf5e7b198d8e5ab5bd5512f1fd14127ac16e8b
Ioana Dunca <dunca.ioana.iulia@gmail.com>
2022-09-01 15:37:56 +0300

    add package for english words

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() {
+  runApp(const XylophoneApp());
+}
+
+class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body:SafeArea(
+          child:Center(child: Text(nouns.last)),
+        )
+      )
+    );
+  }
+}
+
